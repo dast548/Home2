c330052b657ff01d7d5c1c9625120c1e50c204e7
dast548 <orlovsasha2007@gmail.com>
2026-06-05 19:43:29 +0000

    added comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,10 @@
 int main(int argc, char** argv)
 {
   std::string name;
+  // запрашиваем имя пользователя
   std::cout << "Enter your name: ";
   std::cin >> name;
+  // выводим приветствие
   std::cout << "Hello world from @" << name << std::endl;
   return 0;
 }
