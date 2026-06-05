e00e164d0c3f48dc2f3a554d1ef37dea45e62107
dast548 <orlovsasha2007@gmail.com>
2026-06-05 19:41:37 +0000

    fixed code style: removed using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-using namespace std;
 
 int main(int argc, char** argv)
 {
-  string name;
-  cout << "Enter your name: ";
-  cin >> name;
-  cout << "Hello world from @" << name << endl;
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from @" << name << std::endl;
   return 0;
 }
